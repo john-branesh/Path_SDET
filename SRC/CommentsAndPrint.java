e18a37670aa7be533b54de251039ec4e805403b6
john-branesh <johnbraneshherin@gmail.com>
2025-03-10 07:51:24 +0530

    prog about comment, println and print

---
+++ SRC/CommentsAndPrint.java
@@ -0,0 +1,11 @@
+public class CommentsAndPrint {
+    public static void main (String[] args){
+        //a simple println line using println
+        System.out.println("Println will add next line");
+        //Command+D will duplicate above line
+        System.out.println("Println will add next line");
+        //now only print keyword will be used instead of println
+        System.out.print("print will not add next line ");
+        System.out.print("print will not add next line");
+    }
+}
